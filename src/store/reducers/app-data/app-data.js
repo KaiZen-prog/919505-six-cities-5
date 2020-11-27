dacb1d3d1ed166d4588a5693bbf90d3f8605e7e7
Николай Карнаев <karnaev.nick@gmail.com>
2020-11-27 19:56:49 +0300

    Тесты редьюсеров

--- src/store/reducers/app-data/app-data.js
+++ src/store/reducers/app-data/app-data.js
@@ -78,14 +78,17 @@ const appData = (state = initialState, action) => {
       return extend(state, {
         favoriteOffers: removeItem(state.favoriteOffers, action.payload)
       });
+
     case ActionType.CHANGE_OFFERS_FAVORITE_STATUS:
       return extend(state, {
         offers: replaceItem(state.offers, action.payload)
       });
+
     case ActionType.CHANGE_NEARBY_OFFERS_FAVORITE_STATUS:
       return extend(state, {
         nearbyOffers: replaceItem(state.nearbyOffers, action.payload)
       });
+
     case ActionType.CHANGE_OFFER_FAVORITE_STATUS:
       return extend(state, {
         offerDetails: action.payload
